bde0e33bf741ca86db4540bc3f639956ae68ca4f
chrn24 <chrn-24@outlook.jp>
2025-07-19 23:08:15 +0900

    07192308

--- resources/views/admin/shops/editconfirm.blade.php
+++ resources/views/admin/shops/editconfirm.blade.php
@@ -14,7 +14,6 @@
     <input type="hidden" name="price_min" value="{{ $input['price_min'] }}">
     <input type="hidden" name="price_max" value="{{ $input['price_max'] }}">
     <input type="hidden" name="business_hours" value="{{ $input['business_hours'] }}">
-    <input type="hidden" name="business_period" value="{{ $input['business_period'] }}">
     <input type="hidden" name="closed_day" value="{{ $input['closed_day'] }}">
     <input type="hidden" name="zip_code" value="{{ $input['zip_code'] }}">
     <input type="hidden" name="address" value="{{ $input['address'] }}">
@@ -28,7 +27,6 @@
             <tr><th>金額下限</th><td>{{ $input['price_min'] }} 円</td></tr>
             <tr><th>金額上限</th><td>{{ $input['price_max'] }} 円</td></tr>
             <tr><th>営業時間</th><td>{{ $input['business_hours'] }}</td></tr>
-            <tr><th>営業期間</th><td>{{ $input['business_period'] }}</td></tr>
             <tr><th>店休日</th><td>{{ $input['closed_day'] }}</td></tr>
             <tr><th>郵便番号</th><td>{{ $input['zip_code'] }}</td></tr>
             <tr><th>住所</th><td>{{ $input['address'] }}</td></tr>
